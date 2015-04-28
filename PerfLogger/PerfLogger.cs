471212413b9947e027a462ff6a8a73299ab50081
Kostarev Dmitry <albemuth1221@gmail.com>
2015-04-28 12:00:15 +0500

    Second task completed

---
+++ PerfLogger/PerfLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfLogger
+{
+    class PerfLogger : IDisposable
+    {
+        private Stopwatch sw;
+        private Action<int> resultManager;
+
+        private PerfLogger(Action<int> resultManager)
+        {
+            this.resultManager = resultManager;
+            sw = new Stopwatch();
+            sw.Start();
+        }
+
+        public static IDisposable Measure(Action<int> timeManager )
+        {
+            return new PerfLogger(timeManager);
+        }
+
+        public void Dispose()
+        {
+            resultManager(sw.Elapsed.Milliseconds);
+        }
+    }
+}
